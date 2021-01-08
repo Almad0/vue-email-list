4e1f2f64832e4f8bbc0a79e982b28ed7836a5a36
Alessandro <internicola.alessandro@gmail.com>
2021-01-08 16:12:33 +0100

    Esercizio completato
    Utilizzato in ciclo for per importare singolarmente ogni email e caricarlasu un array

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ let root = new Vue({
   el: "#root",
 
   data: {
+    listEmails: []
+  },
 
+  mounted(){
+    for (var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(response => {
+        this.listEmails.push(response.data.response);
+      });
+    };
   },
 
   methods: {
-
   }
 
 });
